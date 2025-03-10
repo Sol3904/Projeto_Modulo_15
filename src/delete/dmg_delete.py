4d6177dd2ef3c41f82f2488893ef01fff5a56a22
Sol3904 <solanumofthequantummoon@gmail.com>
2025-03-10 11:24:58 +0000

    Update dmg_delete.py

--- src/delete/dmg_delete.py
+++ src/delete/dmg_delete.py
@@ -2,7 +2,7 @@
 import sqlite3
 
 #estabelecer conexao
-conex = sqlite3.connect('C:\\Users\\solan\\Desktop\\projetom15\\projeto_modulo_15\\sqlite_db\\sqlite.db')
+conex = sqlite3.connect('..\\..\\sqlite_db\\sqlite.db')
 cursor_dmg = conex.cursor()
 
 #solicita ao utilizador o texto correspondete a linha que vai ser apagada
